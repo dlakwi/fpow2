8cc427626d2d05e6785d1b31dd8f2688c372fb15
Donald Johnson <31147085+dlakwi@users.noreply.github.com>
2021-01-01 10:19:26 -0600

    Update fpow2.h

--- src/fpow2.h
+++ src/fpow2.h
@@ -1,5 +1,23 @@
 // fpow2.h
 
+// from util-C.h in the ETE Toolkit
+// https://github.com/etetoolkit/ext_apps/blob/master/src/clustal-omega-1.2.1/src/hhalign/util-C.h
+// https://github.com/etetoolkit/ext_apps
+
+/*********************************************************************
+ * Clustal Omega - Multiple sequence alignment
+ *
+ * Copyright (C) 2010 University College Dublin
+ *
+ * Clustal-Omega is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This file is part of Clustal-Omega.
+ *
+ ********************************************************************/
+
 #ifndef _FPOW2_H
 #define _FPOW2_H
 
